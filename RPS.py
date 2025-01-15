199a437a8a51d7cc5046c4838a2db750d6c26fe3
KumaCatz K <tod.kagan@gmail.com>
2025-01-15 00:30:20 +0000

    feat: quincy win

--- RPS.py
+++ RPS.py
@@ -1,10 +1,14 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
 def player(prev_play, opponent_history=[]):
+    if prev_play == '':
+        opponent_history.clear()
+        
     opponent_history.append(prev_play)
 
-    guess = "R"
     if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+        guess = {"R": "P", "P": "S", "S": "R"}[opponent_history[-1]]
+    else:
+        guess = "R"
 
     return guess
