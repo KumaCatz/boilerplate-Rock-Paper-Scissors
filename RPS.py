4adc18f5dfb6db0f22a9866b6a5636f10a0c0256
KumaCatz K <tod.kagan@gmail.com>
2025-01-15 18:04:42 +0000

    chore: keras install

--- RPS.py
+++ RPS.py
@@ -1,13 +1,25 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
 def player(prev_play, opponent_history=[]):
+    opponent_history.append(prev_play)
     if prev_play == '':
         opponent_history.clear()
-        
-    opponent_history.append(prev_play)
 
-    if len(opponent_history) > 2:
-        guess = {"R": "P", "P": "S", "S": "R"}[opponent_history[-1]]
+    ideal_response = {"R": "P", "P": "S", "S": "R"}
+
+    count_history = {
+        k: opponent_history.count(k)
+        for k in set(opponent_history)
+        }
+    
+    most_played = max(count_history, key=count_history.get)
+
+    if len(opponent_history) > 1:
+        # with one of the opponents, the guess is going S -> P -> R, in like a cycle
+        # i want to detect this and predict the input
+        
+
+        guess = ideal_response[opponent_history[-1]]
     else:
         guess = "R"
 
